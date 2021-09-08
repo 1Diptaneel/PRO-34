d6cfa0fc8de44799cf2b20f814771faf2e367bc9
1Diptaneel <83410205+1Diptaneel@users.noreply.github.com>
2021-09-08 13:09:28 +0530

    Update Monster.js

--- Monster.js
+++ Monster.js
@@ -9,17 +9,18 @@ class Monster {
 		this.x=x;
 		this.y=y;
 		this.r=r;
-		this.image=loadImage("monster.png");
-		this.body=Bodies.circle(this.x, this.y, (this.r)/2, options)
+		this.image=loadImage("monster1.png");
+		this.body=Bodies.circle(this.x, this.y, (this.r)/2, options);
 		World.add(world, this.body);
 
 	}
+	
 	display()
 	{
 			
 			var monsterPos=this.body.position;		
 			push()
-			translate(monsterPos.x, monsterPos.y-50);
+			translate(monsterPos.x, monsterPos.y-100);
 			rectMode(CENTER)
 			fill(255,0,255)
 			imageMode(CENTER);
